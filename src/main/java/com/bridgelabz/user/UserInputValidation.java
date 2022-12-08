28e5f011bcd560c4f7c60cf871bdc7566cc4cb70
komalarao <116417072+komalarao@users.noreply.github.com>
2022-12-08 09:55:27 -0800

    Update UserInputValidation.java

--- src/main/java/com/bridgelabz/user/UserInputValidation.java
+++ src/main/java/com/bridgelabz/user/UserInputValidation.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.user;
 
 public class UserInputValidation {
+import java.util.regex.Matcher;
+	import java.util.regex.Pattern;
 
+	public class UserInputValidation {
+		// Creating isValidFirstName method to validate the first name given by user using regex
+			  //return - true or false
+			    public static boolean isValidFirstName(String firstName) {
+
+			        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";  //Declaring regex pattern to check the firstName
+
+			        Pattern pattern = Pattern.compile(firstNameRegex);
+			        Matcher matcher = pattern.matcher(firstName);
+
+			        return matcher.matches();
+			    }
 }
