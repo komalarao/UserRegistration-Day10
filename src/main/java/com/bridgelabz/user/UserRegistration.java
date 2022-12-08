a7f7739a06e8cd52e272250bff73ede9d91ebac7
komalarao <manoharvakamullu1999@gmail.com>
2022-12-08 07:28:30 -0800

    add: program for first name pattern test case

--- src/main/java/com/bridgelabz/user/UserRegistration.java
+++ src/main/java/com/bridgelabz/user/UserRegistration.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.user;
 
-public class UserRegistration {
 
-}
+	import java.util.Scanner;
+	public class UserRegistration {
+		public static void main(String[] args) {
+			UserInputValidation userInputValidation = new UserInputValidation();
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter first name: ");
+	        String firstName = scanner.next();
+	        System.out.println(userInputValidation.isValidFirstName(firstName) ? "Valid" : "In Valid");
+	        scanner.close();
+		}
+	}
