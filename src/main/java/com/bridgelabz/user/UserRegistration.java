868f1e70e8387053be338f6b2fbe3dd4258b229c
komalarao <116417072+komalarao@users.noreply.github.com>
2022-12-08 09:57:00 -0800

    Update UserRegistration.java

--- src/main/java/com/bridgelabz/user/UserRegistration.java
+++ src/main/java/com/bridgelabz/user/UserRegistration.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.user;
 
 public class UserRegistration {
-
+public static void main(String[] args) {
+			UserInputValidation userInputValidation = new UserInputValidation();
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter first name: ");
+	        String firstName = scanner.next();
+	        System.out.println(userInputValidation.isValidFirstName(firstName) ? "Valid" : "In Valid");
+	        scanner.close();
+		}
 }
